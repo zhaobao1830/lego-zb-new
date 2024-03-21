7a99ef43c16a24df1376ec864e1f049c2e0ae403
zhaobao1830 <373098777@qq.com>
2024-03-21 09:01:11 +0800

    初始化项目

---
+++ src/views/home/home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+</template>
+
+<script setup lang="ts">
+  import { ref } from 'vue'
+
+  const test = ref('')
+  console.log(test)
+</script>
+
+<style scoped lang="scss">
+
+</style>
