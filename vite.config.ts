66e2ebec69283b2e4a385f97af7f9f4861c97295
zhaobao1830 <373098777@qq.com>
2024-03-21 09:19:14 +0800

    修改tsconfig.json文件的配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    },
+    // 导入时想要省略的扩展名列表
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
+  }
 })
